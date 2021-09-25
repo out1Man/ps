3c360a38096e1a96c1fe4d9958cf0adcd3add999
out1Man <2448092139@qq.com>
2021-09-25 21:26:48 +0800

    9.25

---
+++ code/第一个任务.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <math.h>
+int main()
+{
+	FILE* a = fopen("C:\\Users\\out Man\\Desktop\\ps后端\\task1\\test2.png", "rb");
+	fseek(a, 8, 0);//跳过文件开头
+	while (1)
+	{
+		unsigned char b[200] = { 0 };
+		unsigned char c[200] = { 0 };
+		int x = fread(b, 1, 4, a);//获取长度			   
+		int y = fread(c, 1, 4, a);//获取类型
+		c[x] = '\0';
+		c[y] = '\0';
+		printf("类型为%s ", c);
+		int n = (b[0] << 24) + (b[1] << 16) + (b[2] << 8) + b[3];
+		printf("长度为%6d ", n);
+		fseek(a, n, 1);//跳过数据块
+		printf("crc为 ");
+		printf("%3d ", fgetc(a));//crc第一个字节
+		printf("%3d ", fgetc(a));//crc第二个字节
+		printf("%3d ", fgetc(a));
+		printf("%3d ", fgetc(a));
+		printf("\n");
+		if (n==0)
+			break;
+	}
+	fclose(a);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+////qsort函数 万能的快速排序
+////void qsort(void* base,size_t num,size_t width,int (*cmp)(const void* e1,const void* e2))
+//#include <stdio.h>
+//#include <stdlib.h>
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//};
+//int cmp_int(const void* e1, const void* e2)
+//{
+//	return *(int*)e1 - *(int*)e2;
+//}
+//int cmp_float(const void* e1, const void* e2)
+//{
+//	return (int)(*(float*)e1 - *(float*)e2);
+//}
+//int cmp_stu(const void* e1, const void* e2)
+//{
+//	return (((struct stu*)e1)->age) - (((struct stu*)e2)->age);
+//}
+//void test1()
+//{
+//	int arr1[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	qsort(arr1, sizeof(arr1) / sizeof(arr1[0]), sizeof(arr1[0]), cmp_int);
+//	int j = 0;
+//	for (j = 0; j < sizeof(arr1) / sizeof(arr1[0]); j++)
+//		printf("%d ", arr1[j]);
+//	printf("\n");
+//}
+//void test2()
+//{
+//	float arr2[] = { 9.0,8.0,7.0,6.0,4.0,3.0 };
+//	qsort(arr2, sizeof(arr2) / sizeof(arr2[0]), sizeof(arr2[0]), cmp_int);
+//	int j = 0;
+//	for (j = 0; j < sizeof(arr2) / sizeof(arr2[0]); j++)
+//		printf("%f ", arr2[j]);
+//}
+//void test3()
+//{
+//	struct stu s[3] = { {"zhangsan",20},{"lisi",10},{"wuhan",50} };
+//	qsort(s, sizeof(s) / sizeof(s[0]), sizeof(s[0]), cmp_stu);
+//	
+//}
+//int main()
+//{
+//	test1();
+//	test2();
+//	test3();
+//	
+//	
+//	return 0;
+//}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 第三个任务/任务3.py
@@ -0,0 +1,95 @@
+import zlib
+import numpy
+
+filename=input("请输入文件位置")
+f=open(filename,"rb")
+png=f.read()
+png_list=list(bytearray(png))
+length=(2 ** 24) * png_list[16] + \
+                 (2 ** 16) * png_list[17] + \
+                 (2 ** 8) * png_list[18] + \
+                 (2 ** 0) * png_list[19]
+height=(2 ** 24) * png_list[20] + \
+                 (2 ** 16) * png_list[21] + \
+                 (2 ** 8) * png_list[22] + \
+                 (2 ** 0) * png_list[23]
+print("height: ",height)
+print("length: " ,length)
+
+
+
+nu_byte=8
+
+by_idat=b""
+
+while nu_byte<len(png_list):
+    length2=(2 ** 24) * png_list[nu_byte] + \
+                 (2 ** 16) * png_list[nu_byte + 1] + \
+                 (2 ** 8) * png_list[nu_byte + 2] + \
+                 (2 ** 0) * png_list[nu_byte + 3]
+    b_data={"length2":length2,
+            "type":png[nu_byte+4:nu_byte+8],
+            "crc":png_list[nu_byte+8+length2:nu_byte+12+length2]}
+    if b_data["type"].decode()=='IDAT':
+        idat=png[nu_byte+8:nu_byte+8+length2]
+        by_idat += idat
+    nu_byte+=12+length2
+
+
+uncom=zlib.decompress(by_idat)
+#print(uncom[0:100])
+rlist=[]
+glist=[]
+blist=[]
+rgblist=list(uncom)
+for i in range(height):
+    if rgblist[i * (length * 3+1 )] == 0:
+        continue
+    elif rgblist[i * (length * 3 +1)] == 1:
+        for j in range(4,3*length+1):
+            rgblist[i * (length*3+1) + j] = int((rgblist[i * (length*3+1) + j] + rgblist[i * (length*3+1) + j - 3]) % 256)
+    elif rgblist[i * (length * 3 +1)] == 2:
+        for j in range(1,3*length+1):
+           rgblist[i * (length*3+1) + j] = int((rgblist[(i - 1) * (length*3+1) + j] + rgblist[i * (length*3+1) + j]) % 256)
+    elif rgblist[i * (length * 3 +1)] == 3:
+        for j in range(4,3*length+1):
+            rgblist[i * (length*3+1) + j] = int((rgblist[(i - 1) * (3*length+1) + j] + rgblist[i * (length*3+1) + j - 3]) / 2 + rgblist[i * (length*3+1) + j]) % 256
+    elif rgblist[i * (length * 3 +1)] == 4:
+        for j in range(4,3*length+1):
+            a = rgblist[j - 3 + i * (length*3+1)]
+            b = rgblist[j + (i - 1) * (length*3+1)]
+            c = rgblist[j - 3 + (i - 1) * (length*3+1)]
+            p = a + b - c
+            pa = abs(p - a)
+            pb = abs(p - b)
+            pc = abs(p - c)
+            if pa <= pb and pa <= pc:
+                count = a
+            elif pb <= pc:
+                count = b
+            else:
+                count = c
+            rgblist[j + i * (length*3+1)] = int((rgblist[j + i * (length*3+1)] + count) % 256)
+
+for i in range(height):
+    for j in range(1,length*3+1):
+        if j%3==1:
+            rlist.append(rgblist[i*(length*3+1)+j])
+        elif j%3==2:
+            glist.append(rgblist[i*(length*3+1)+j])
+        elif j%3==0:
+            blist.append(rgblist[i * (length * 3 + 1) + j])
+
+
+while 1:
+    x = int(input(("请输入x")))
+    y = int(input(("请输入y")))
+    print("r:",257*rlist[x*length+y])
+    print("g:",257*glist[x*length+y])
+    print("b:",257*blist[x*length+y])
+
+
+
+
+
+
